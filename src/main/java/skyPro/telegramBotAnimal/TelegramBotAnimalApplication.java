477aa20102200dc825000a8ab842113309b3d824
romariofutbolist <66all@mail.ru>
2024-06-14 22:14:26 +0300

    Проверка работоспособности

---
+++ src/main/java/skyPro/telegramBotAnimal/TelegramBotAnimalApplication.java
@@ -0,0 +1,13 @@
+package skyPro.telegramBotAnimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotAnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotAnimalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skyPro/telegramBotAnimal/TelegramBotAnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package skyPro.telegramBotAnimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotAnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
